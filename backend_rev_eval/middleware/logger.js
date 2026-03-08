c7a6d1b742f98d860f2d8af6a353b44fab8f2252
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-03-08 19:45:58 +0530

    backend revision assignment submitted

--- backend_rev_eval/middleware/logger.js
+++ backend_rev_eval/middleware/logger.js
@@ -1,7 +1,7 @@
 
 export const logger = (res,req,next) => {
 
-    console.log(`[{$(new Date().toISOstring()} ${req.method} ${req.originurl}]`)
+    console.log(`[{$(new Date().toISOstring())} ${req.method} ${req.originurl}]`)
 
 next();
 }
--- backend_rev_eval/middleware/validateEnrollment.js
+++ backend_rev_eval/middleware/validateEnrollment.js
@@ -1,22 +1,21 @@
 import supabase from "../supabaseClient";
 
-const validateEnrollment =(res,req,next)=>{
+const validateEnrollment =(req,res,next)=>{
 const {student_id,course_id}=req.body
 try{
-if(!student_id) res.status(400).res.json({message:"student id is required"})
-if(!course_id) res.status(400).res.json({message:"couse id is required"})
+if(!student_id) 
+    return res.status(400).res.json({message:"student id is required"})
+if(!course_id) 
+    return res.status(400).res.json({message:"couse id is required"})
 
 }catch{
     if (error){
-        console.log(err)
+        console.log(error)
     res.status(500).res.json({message:"server error"})
 }
 }
 
 next();
-
 }
-
-
 
 export default validateEnrollment
